981f3401a2bfb0b0590f426195cfa12847bd852e
ak358 <arikana.358code@gmail.com>
2024-05-11 16:04:47 +0900

    カテゴリーを追加しました

---
+++ PhotoApp_MVC/Models/Category.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PhotoApp_MVC.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [ValidateNever]
+        public User User { get; set; }
+        [ValidateNever]
+        public List<PhotoPost> PhotoPosts { get; set; }
+    }
+}
